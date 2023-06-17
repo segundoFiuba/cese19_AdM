f6b33f6bc1dafeb85b539d49859a0275f5676a64
segundoFiuba <smolinaa@fi.uba.ar>
2023-06-17 01:14:07 -0300

    Ejercicios

--- adm_c16/Core/Inc/main.h
+++ adm_c16/Core/Inc/main.h
@@ -107,6 +107,7 @@ void Error_Handler(void);
 #define RMII_TXD0_GPIO_Port GPIOG
 #define LD2_Pin GPIO_PIN_7
 #define LD2_GPIO_Port GPIOB
+
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
